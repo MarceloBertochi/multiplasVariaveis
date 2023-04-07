1d45ef6eb9b42b563df64d3e53ad59d4d8a34362
MarceloBertochi <paidasamia@gmail.com>
2023-04-07 20:39:36 -0300

    Multiplas Variáveis

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace multiplasVariaveis
+
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+
+        {
+            string nome = "Marcelo ", sobrenome = "Bertochi";
+
+            int x = 6, y = 10, z = 3;
+
+            Console.WriteLine(nome + sobrenome);
+            Console.WriteLine();
+            Console.WriteLine(x + y + z);
+            Console.WriteLine();
+
+            int a, b, c;
+            a = b = c = 50;
+            Console.WriteLine(a + b + c);
+
+        }
+    }
+}
